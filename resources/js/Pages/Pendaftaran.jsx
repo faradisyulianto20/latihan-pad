1960ea3ac0ffc3db3862d2ed64d207627188e55c
Faradis Yulianto <faradisy20@gmail.com>
2025-08-29 18:09:14 +0700

    [feat] add shadcn

--- resources/js/Pages/Pendaftaran.jsx
+++ resources/js/Pages/Pendaftaran.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+import {Switch} from "../components/ui/switch";
+
 export default function Pendaftaran() {
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Halaman Pendaftaran</h1>
             <p>Ini adalah halaman pendaftaran.</p>
+            <Switch />
         </div>
     );
-}
+} 
