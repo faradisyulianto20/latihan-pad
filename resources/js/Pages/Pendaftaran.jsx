d44cf847dac8bb1b12a676b30e76fb37a8111adb
Faradis Yulianto <faradisy20@gmail.com>
2025-08-29 19:47:39 +0700

    [feat] add register page

--- resources/js/Pages/Pendaftaran.jsx
+++ resources/js/Pages/Pendaftaran.jsx
@@ -1,13 +1,133 @@
 import React from "react";
 
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import {Switch} from "../components/ui/switch";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { ModeToggle } from "@/Components/mode-toggle";
+
+const FormSchema = z.object({
+    username: z.string().min(2, { message: "Username must be at least 2 characters." }),
+    nim: z.string().min(2, { message: "NIM must be at least 2 characters." }),
+    alamat: z.string().min(2, { message: "Alamat must be at least 2 characters." }),
+    orangtua: z.string().min(2, { message: "Orang Tua must be at least 2 characters." }),
+});
+
 export default function Pendaftaran() {
+    const form = useForm({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            username: "",
+            nim: "",
+            alamat: "",
+            orangtua: "",  
+        },
+    })
+    function onSubmit(data) {
+        toast("You submitted the following values", {
+        description: (
+            <pre className="mt-2 w-[320px] rounded-md bg-neutral-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+            </pre>
+        ),
+        })
+    }
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Halaman Pendaftaran</h1>
-            <p>Ini adalah halaman pendaftaran.</p>
-            <Switch />
-        </div>
+        <>
+            <div className="p-4 flex justify-end">
+                <ModeToggle/>
+            </div>
+            <div className="container mx-auto p-4">
+                <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl font-bold mb-4">Pendaftaran</CardTitle>
+                    <CardDescription classNamemb-4>Isilah formulir-formulir berikut!</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-6">
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nama</FormLabel>
+                            <FormControl>
+                            <Input placeholder="Nama" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="nim"
+                        render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>NIM</FormLabel>
+                            <FormControl>
+                            <Input placeholder="NIM" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                        )}
+                    />
+                        <FormField
+                            control={form.control}
+                            name="alamat"
+                            render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Alamat</FormLabel>
+                                <FormControl>
+                                <Input placeholder="Alamat" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="orangtua"
+                            render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Orang Tua</FormLabel>
+                                <FormControl>
+                                <Input placeholder="Orang Tua" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                            )}
+                        />
+                            <Button type="submit">Daftar</Button>
+                        </form>
+                    </Form>
+                </CardContent>
+                </Card> 
+            </div>
+        </>
+        
     );
 } 
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot, hydrateRoot } from 'react-dom/client';
+import { ThemeProvider } from "@/components/theme-provider"; // Import ThemeProvider
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -15,12 +16,18 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.jsx'),
         ),
     setup({ el, App, props }) {
+        const element = (
+            <ThemeProvider>
+                <App {...props} />
+            </ThemeProvider>
+        );
+
         if (import.meta.env.SSR) {
-            hydrateRoot(el, <App {...props} />);
+            hydrateRoot(el, element);
             return;
         }
 
-        createRoot(el).render(<App {...props} />);
+        createRoot(el).render(element);
     },
     progress: {
         color: '#4B5563',
