3b51ba75ef42422224a6417471e46ad89fa4fa0b
Fahmi Haqqul Ihsan <fahmihaqqulihsan@gmail.com>
2025-09-02 01:35:03 +0700

    [refactor] fixing Toast pop up

--- resources/js/Layouts/MainLayouts.jsx
+++ resources/js/Layouts/MainLayouts.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Navbar from '@/Components/Navbar';
-import ModeToggle from '@/Components/mode-toggle';
+import React from "react";
+import Navbar from "@/Components/Navbar";
+import ModeToggle from "@/Components/mode-toggle";
+import { Toaster } from "sonner";
 
 export default function MainLayout({ children }) {
-  return (
-    <div>
-        <div className="fixed bottom-4 left-4">
-            <ModeToggle/>
+    return (
+        <div>
+            <div className="fixed bottom-4 left-4">
+                <ModeToggle />
+            </div>
+            <Navbar />
+            <main className="pt-[100px] px-6">{children}</main>
+            <Toaster richColors position="top-right" />
         </div>
-      <Navbar /> 
-      <main className="pt-[100px] px-6">
-        {children}
-      </main>
-    </div>
-  );
+    );
 }
